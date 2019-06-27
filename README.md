e2e4a8825e076fe54889276cffaffd2a0e786518
Meas Molika <measmolika9@gmail.com>
2019-06-27 17:14:01 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cli_array_calculator
+###Usage:
+calc add <num1> <num2> ... <num>
+calc substract <num1> <num2> ... <num>
+calc multiply <num1> <num2> ... <num>
+calc divide <num1> <num2> ... <num>
+
---
+++ cli.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const [,, ... args] = process.argv
+const help = "[USAGE]:\n"+
+"calc add <num1> <num2> ... <num>\n"+
+"calc substract <num1> <num2> ... <num>\n"+
+"calc multiply <num1> <num2> ... <num>\n"+
+"calc divide <num1> <num2> ... <num>"
+
+var arr = [];
+for(var i=1; i<args.length; i++) {
+	var n = parseFloat(args[i])
+	arr.push(n)
+}
+
+var res 
+
+if(args.length >= 2) {
+   res = arr[0]
+   switch(args[0]) {
+	case "add":
+		for (var i = 1; i < arr.length; i++) {
+			res += arr[i]		
+		}
+		console.log(`result is ${res.toFixed(2)}`)
+		break
+	case "substract":
+		for (var i = 1; i < arr.length; i++) {
+			res -= arr[i]
+		}
+		console.log(`result is ${res.toFixed(2)}`)
+		break
+	case "multiply":
+                for (var i = 1; i < arr.length; i++) {
+                        res *= arr[i]
+                }
+		console.log(`result is ${res.toFixed(2)}`)
+		break
+	case "divide":
+                for (var i = 1; i < arr.length; i++) {
+                        res /= arr[i]
+                }		
+		console.log(`result is ${res.toFixed(2)}`)
+		break
+	default :
+		console.log("Invalid Argument")
+		break
+   }
+} else {
+	console.log(help)
+}
